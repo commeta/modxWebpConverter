e8a3af1efb568961bb5a95ebf776ac7661ad4d40
Евгений <40506054+commeta@users.noreply.github.com>
2020-08-16 05:56:25 +0300

    Update webp_converter.php

--- webp_converter.php
+++ webp_converter.php
@@ -1,5 +1,6 @@
 <?php
 // Modx revo plugin: convert jpg and png images to webp in admin panel
+// Добавляет индикатор в левом верхнем меню. Осуществляет конвертацию изображений в формат webp.
 switch ($modx->event->name) {
     case 'OnManagerPageBeforeRender':
         $modx->controller->addJavascript('/connectors/converter/converter.js');
